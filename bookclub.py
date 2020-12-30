0ec4a83cba21cfab0c955ea23b9109c30ef6f9d5
zhaboots <zhaboots@users.noreply.github.com>
2020-12-30 17:09:46 +0800

    Add counter looping back to 1

--- bookclub.py
+++ bookclub.py
@@ -7,14 +7,12 @@ with open("taleof2cities.txt","r") as f:
 
 with open("counter.txt","r") as f2:
     counter = json.load(f2)
-    counter += 1
-    if counter > len(chapters):
-        counter = 1
 
 with open("counter.txt","w") as f2:
+    counter += 1
+    if counter > len(chapters)-1:
+        counter = 1
     json.dump(counter,f2)
-
-
 
 print(chapters[counter])
 
